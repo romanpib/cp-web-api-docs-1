5d9aa337596873e30a363b52cd7b1687291ed52b
Damian Anslik <damiananslik@outlook.com>
2022-05-20 09:15:00 +0100

    add sidebar and set up content area

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,31 @@
 
 <template>
   <Sidenav />
-  <router-view/>
+  <main>
+    <!-- This is where the views are injected -->
+    <router-view />
+  </main>
 </template>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  
+  display: flex;
+  flex-direction: row;
+}
+
+/* 300px is the width of the sidenav */
+/* flex 1 fills in the remaining width */
+main {
+  flex: 1;
+  margin-left: 300px;
+  padding-inline: 50px;
 }
 </style>
--- src/components/Sidenav.vue
+++ src/components/Sidenav.vue
@@ -1,27 +1,57 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/quickstart">Quickstart</router-link> |
-    <router-link to="/authentication">Authentication</router-link> |
-    <router-link to="/endpoints">Endpoint Explorer</router-link> |
-    <router-link to="/websockets">Websockets</router-link> |
-    <router-link to="/workflows">Workflows</router-link> |
-    <router-link to="/changelog">Changelog</router-link> |
+    <div id="brand">
+      <img src="@/assets/ib-logo-text-black.svg" alt="ibkr-logo">
+      <h6>Client Portal API Documentation</h6>
+    </div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/quickstart">Quickstart</router-link>
+    <router-link to="/authentication">Authentication</router-link>
+    <router-link to="/endpoints">Endpoint Explorer</router-link>
+    <router-link to="/websockets">Websockets</router-link>
+    <router-link to="/workflows">Workflows</router-link>
+    <router-link to="/changelog">Changelog</router-link>
     <router-link to="/support">Support</router-link>
   </nav>
 </template>
 
 <style>
 nav {
-  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(196, 196, 196, 0.25);
+  text-align: left;
+  width: 300px;
+  height: 100vh;
+  position: fixed;
+  overflow: hidden;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  padding-block: 0.75rem;
+  padding-left: 1.5rem;
+  text-decoration: none;
+  color: black;
+}
+
+nav a:hover {
+  color: #db1222;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: #db1222;
+  color: white;
+}
+
+h6 {
+  font-weight: bold;
+  margin: 0;
+  background-color: unset;
+}
+
+#brand {
+  width: 75%;
+  padding-left: 1.5rem;
+  padding-block: 1rem;
 }
 </style>
