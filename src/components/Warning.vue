f3498545eaec03a465741ffb3a0a8065e58195c1
Damian Anslik <damiananslik@outlook.com>
2022-05-24 11:27:28 +0100

    support page

---
+++ src/components/Warning.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        props: {
+            message: {type: String, required: true}
+        }
+    }
+</script>
+
+<template>
+    <div class="warning-message">
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+    .warning-message {
+        text-align: center;
+        background-color: #FFC107;
+        padding: 1.5rem;
+    }
+</style>
