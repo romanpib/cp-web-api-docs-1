a89ab32f6661ab62000f79bd9961817c95de6bd9
Damian Anslik <damiananslik@outlook.com>
2022-05-24 10:44:42 +0100

    home page

---
+++ src/views/BaseView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="content">
+            <slot name="content"></slot>
+        </div>
+        <div class="aside">
+            <slot name="aside"></slot>
+        </div>
+    </div>
+</template>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
+        gap: 50px;
+        padding-inline: 50px;
+    }
+
+    .content {
+        flex: 2;
+    }
+
+    .aside {
+        flex: 1;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <script>
-  import ExpandableCard from '../components/ExpandableCard.vue'
-  import LatestUpdateCard from '../components/LatestUpdateCard.vue'
+  import BaseView from './BaseView.vue'
+  import ExpandableCard from '@/components/ExpandableCard.vue'
+  import LatestUpdateCard from '@/components/LatestUpdateCard.vue'
   export default {
     components: {
+      BaseView,
       ExpandableCard,
       LatestUpdateCard
     },
@@ -55,8 +57,8 @@
 </script>
 
 <template>
-  <div class="container">
-    <div class="content">
+  <BaseView>
+    <template #content>
       <h2>Client Portal Web API Documentation</h2>
       <p>
         Interactive Brokers offers the ability to trade, monitor and manage your IB account using a single RESTful API.
@@ -64,43 +66,31 @@
       <p>
         With the Client Portal Web API:
       </p>
-      <ul id="features-list">
+      <ul class="features-list">
         <li>There is no need for special libraries to integrate with your application.</li>
         <li>You use only a small, headless gateway to connect to your application.</li>
         <li>Additional security with IP restrictions is available.</li>
         <li>Enjoy access to data that's not available through our existing APIs, including PortfolioAnalyst, Statements, Transaction History (up to 90 days) and more.</li>
       </ul>
       <ExpandableCard v-for="card in cards" :title="card.title" :content="card.content" />
-    </div>
-    <div class="latest-changes">
-      <div class="header">
+    </template>
+    <template #aside>
+      <div class="latest-changes-header">
         <h2>Latest Updates</h2>
         <router-link to="/changelog">Changelog</router-link>
       </div>
       <LatestUpdateCard v-for="update in updates" :title="update.title" :content="update.content" :updateDate="update.updateDate" />
-    </div>
-  </div>
+    </template>
+  </BaseView>
 </template>
 
 <style>
-  .container {
-    display: flex;
-    flex-direction: row;
-    gap: 5rem;
-    padding-inline: 50px;
+  .features-list {
+    margin-bottom: 2rem;
+    line-height: 2rem;
   }
 
-  .container .content {
-    width: 100%;
-    flex: 2;
-  }
-
-  .container .latest-changes {
-    flex: 1;
-    text-align: left;
-  }
-
-  .latest-changes .header {
+  .latest-changes-header {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -108,15 +98,10 @@
     gap: 10px;
   }
 
-  .latest-changes .header a {
+  .latest-changes-header a {
     text-decoration: underline;
     color: #db1222;
     font-size: small;
     font-weight: bold;
-  }
-
-  #features-list {
-    margin-bottom: 2rem;
-    line-height: 2rem;
   }
 </style>
