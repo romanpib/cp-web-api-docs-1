478afa87e9f4a5ce75ec4e87a5c01eb112158dcc
damian <danslik@interactivebrokers.ie>
2022-06-20 11:59:36 +0100

    update gitignore and vue config

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ module.exports = {
         entry: 'src/main.js',
         title: 'Client Portal Web API Documentation',
       },
-    }
+    },
+    publicPath: process.env.NODE_ENV === "production" ? "/cp-web-api-docs/" : ""
   }
