81908553c445a03c144bc64d65e4b7b026506abc
damian <danslik@interactivebrokers.ie>
2022-06-20 11:38:27 +0100

    add support for swagger

---
+++ src/components/endpoints/EndpointsIndividual.vue
@@ -0,0 +1,16 @@
+<script>
+import SwaggerUI from 'swagger-ui';
+import 'swagger-ui/dist/swagger-ui.css'
+export default {
+    mounted() {
+        SwaggerUI({
+            spec: require('@/components/endpoints/swagger.json'),
+            dom_id: '#swagger'
+        })
+    }
+}
+</script>
+
+<template>
+    <div id="swagger"></div>
+</template>
--- src/views/Endpoints.vue
+++ src/views/Endpoints.vue
@@ -1,24 +1,43 @@
 <script>
-import BaseView from "@/views/BaseView.vue";
+import { useAccountTypeStore } from '@/stores/accountTypeStore.js';
+import BaseViewNoAside from "@/views/BaseViewNoAside.vue";
+import AccountToggle from "@/components/AccountToggle.vue";
+import EndpointsIndividual from "@/components/endpoints/EndpointsIndividual.vue"
+import EndpointsInstitutional from "@/components/endpoints/EndpointsInstitutional.vue"
 export default {
   components: {
-    BaseView
+    BaseViewNoAside,
+    AccountToggle,
+    EndpointsIndividual,
+    EndpointsInstitutional
+  },
+  computed: {
+    activeTab() {
+      const store = useAccountTypeStore()
+      return (store.accountType == 'individual') ? EndpointsIndividual : EndpointsInstitutional
+    }
   }
 }
 </script>
 
 <template>
-  <base-view>
+  <base-view-no-aside>
     <template #content>
       <div class="page_header">
-        <h2>Endpoint Explorer</h2>
+        <h2>Endpoints Explorer</h2>
         <account-toggle />
       </div>
-      <p>Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id fermentum sem. Etiam egestas lorem ac elit iaculis dignissim.</p>
+      <p>
+        Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id
+        fermentum sem. Etiam egestas lorem ac elit iaculis dignissim. Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id
+        fermentum sem. Etiam egestas lorem ac elit iaculis dignissim. Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id
+        fermentum sem. Etiam egestas lorem ac elit iaculis dignissim. Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id
+        fermentum sem. Etiam egestas lorem ac elit iaculis dignissim. 
+ 
+      </p>
+      <component :is="activeTab" />
     </template>
-    <template #aside>
-    </template>
-  </base-view>
+  </base-view-no-aside>
 </template>
 
 <style>
