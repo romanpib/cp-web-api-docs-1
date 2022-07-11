2fd6f2a39c17fad2e56fb2d7033b786c4474d80a
damian-anslik <damiananslik@outlook.com>
2022-07-11 14:27:59 +0100

    clear changelog and add new documentation change

--- src/docs/changelog/index.js
+++ src/docs/changelog/index.js
@@ -1,58 +1,16 @@
 const changelog = [
     {
         title: 'New API documentation released',
-        date: '2022-01-01',
-        description: 'Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id fermentum sem.',
+        date: '2022-07-12',
+        description: 'New API documentation released',
         changes: [
-            'This is the first change',
-            'This is a second change',
+            'Released new Client Portal API documentation',
+            'Visual overhaul of the documentation UI',
+            'Grouped topics into relevant sections',
+            'Institutional topics are in progress and will be released during the week'
         ],
         tags: [
-            'market-data',
-            'orders',
-            'authentication'
-        ]
-    },
-    {
-        title: 'New API documentation released',
-        date: '2022-06-16',
-        description: 'This is a change to the API documentation',
-        changes: [
-            'This is the first change',
-            'This is a second change',
-        ],
-        tags: [
-            'market-data',
-            'orders',
-            'portfolio',
-            'account-management'
-        ]
-    },
-    {
-        title: 'New API documentation released',
-        date: '2022-05-31',
-        description: 'Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id fermentum sem.',
-        changes: [
-            'This is the first change',
-            'This is a second change',
-        ],
-        tags: [
-            'orders',
-            'authentication'
-        ]
-    },
-    {
-        title: 'Hello world',
-        date: '2022-06-14',
-        description: 'Quisque ultrices leo quam, sed eleifend mauris bibendum in. Donec molestie vehicula ullamcorper. Maecenas id fermentum sem.',
-        changes: [
-            'This is the first change',
-            'This is a second change',
-        ],
-        tags: [
-            'market-data',
-            'orders',
-            'authentication'
+            'documentation',
         ]
     },
 ]
